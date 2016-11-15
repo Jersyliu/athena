b67c03c4d92422fdcb8fb745aea3ca0faaf83022
Jersyliu <jersyliu@outlook.com>
2016-11-15 16:13:23 -0500

    improvement

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = "homepage"
 
 urlpatterns = [
-        url(r'course/(?P<username>[a-zA-Z0-9+]+)/(?P<coursename>[a-zA-Z0-9+]+)/(?P<lessonname>[ a-zA-Z0-9+]+)/$', views.course, name = 'course'),
-        url(r'otherprofile/(?P<username>[a-zA-Z0-9+]+)/$', views.otherprofile, name = 'otherprofile'),
-        url(r'profile/(?P<username>[a-zA-Z0-9+]+)/$', views.profile, name = 'profile'),
+        url(r'course/(?P<username>[^/]+)/(?P<coursename>[^/]+)/(?P<lessonname>[^/]+)/$', views.course, name = 'course'),
+        url(r'otherprofile/(?P<fromWho>[^/]+)/(?P<toWho>[^/]+)/$', views.otherprofile, name = 'otherprofile'),
+        url(r'profile/(?P<username>[^/]+)/$', views.profile, name = 'profile'),
         url(r'logoutuser/$',views.logoutuser, name = 'logoutuser'),
-        url(r'courselist/(?P<username>[a-zA-Z0-9+]+)/$', views.courseList, name = 'courseList'),  
+        url(r'courselist/(?P<username>[^/]+)/$', views.courseList, name = 'courseList'),  
 ]
 
