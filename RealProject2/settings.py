32be898eb32925d34137cdc05fad646a5c9ce41c
BKrieger73 <bk494@cornell.edu>
2016-11-15 00:13:43 +0000

    changing db to postgres

--- RealProject2/settings.py
+++ RealProject2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'RealProject2.wsgi.application'
 
 DATABASES = {
     'default': {
-		'ENGINE': 'django.db.backends.mysql',
+		'ENGINE': 'django.db.backends.postgres',
 		'NAME': 'athena_db2',
 		'USER': 'root',
 		'PASSWORD': 'crazyworld',
