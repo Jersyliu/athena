8839f49e27e9d98ca9a51f957e24dc4c680b7242
BKrieger73 <bk494@cornell.edu>
2016-11-15 01:01:06 +0000

    change

--- RealProject2/settings.py
+++ RealProject2/settings.py
@@ -20,6 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '_d_*#dey6_y@-$k9e%)ah0#fm&$^f#fx)2q@--wtb2_8x6u%-h'
 
