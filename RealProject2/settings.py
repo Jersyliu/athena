06ee3e599af5a1e1032cfbff3df041f0e4c8634e
Jersyliu <jersyliu@outlook.com>
2016-11-19 01:58:31 -0500

    ff

--- RealProject2/settings.py
+++ RealProject2/settings.py
@@ -136,13 +136,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/login/static/'
+#STATIC_URL = '/login/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'login/static'),
-    os.path.join(BASE_DIR, 'homepage/static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'login/static'),
+#    os.path.join(BASE_DIR, 'homepage/static'),
+#)
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
