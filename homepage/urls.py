8bb1a8fee01241207f0d76415cc78eee81f34935
Jersyliu <jersyliu@outlook.com>
2016-11-20 22:45:12 -0500

    work

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "homepage"
 
 urlpatterns = [
+        url(r'keepnotes/(?P<username>[^/]+)/(?P<lessonname>[^/]+)/$', views.keepnotes, name = 'keepnotes'),
         url(r'chat/(?P<fromID>[^/]+)/(?P<fromName>[^/]+)/(?P<toID>[^/]+)/(?P<toName>[^/]+)$', views.chat, name = 'chat'),
         url(r'keepprogress/(?P<username>[^/]+)/(?P<lessonname>[^/]+)/$', views.keepprogress, name = 'keepprogress'),
         url(r'course/(?P<username>[^/]+)/(?P<coursename>[^/]+)/(?P<lessonname>[^/]+)/$', views.course, name = 'course'),
